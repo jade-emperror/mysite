fbde4af2e0a11b8322604a23661e6f9a05513049
jade-emperror <maestrobharath@gmail.com>
2020-11-26 22:04:25 +0530

    added projects section to the site with mobile and pc being responsive

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
 function throwresume(){
     document.location.href="/rcs/resume.pdf";
 }
+function openau(){
+    var win = window.open("https://coe1.annauniv.edu/home/", '_blank');
+    win.focus();
+}
